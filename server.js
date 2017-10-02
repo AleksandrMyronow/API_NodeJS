1881958f54f0a33558cdd1cc5dae58aa17f5bb64
aleksandrmyronov <a.myronow@gmail.com>
2017-10-02 16:21:32 +0300

    second lesson

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 var artists = [
     {
@@ -17,12 +21,41 @@ var artists = [
     }
 ];
 
+app.get('/artists', function (req, res) {
+    res.send(artists)
+});
+
 app.get('/artists/:id', function (req, res) {
     var artist = artists.find(function (artist) {
         return artist.id === Number(req.params.id);
     });
     res.send(artist);
 });
+
+app.post('/artists', function (req, res) {
+    var newArtist = {
+      id: 4,
+      name: req.body.name
+    };
+    artists.push(newArtist);
+    res.send(newArtist);
+});
+
+app.put('/artists/:id', function (req, res) {
+    var modifyArtist = artists.find(function (artist) {
+        return artist.id === Number(req.params.id)
+    });
+    modifyArtist.artist = req.body.artist;
+    res.sendStatus(200);
+});
+
+app.delete('/artists/:id', function (req, res) {
+    artists = artists.filter(function (artist) {
+        return artist.id !== Number(req.params.id);
+    });
+    res.sendStatus(200);
+});
+
 
 app.listen(3012, function () {
     console.log('API app started')
